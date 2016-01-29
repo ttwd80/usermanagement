7da295c0e07bfdebd5e0730eaa717d09d6a3f0ed
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-01-29 21:38:05 +1100

    proper location for xml context file

--- src/test/java/db/ElasticSearchDbIT.java
+++ src/test/java/db/ElasticSearchDbIT.java
@@ -10,7 +10,7 @@ import com.github.ttwd80.usermanagement.model.repository.UserRepository;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.IsEqual.*;
 
-@ContextConfiguration(locations = { "classpath:spring-properties.xml", "classpath:" })
+@ContextConfiguration(locations = { "classpath:spring-properties.xml", "classpath:spring-elasticsearch.xml" })
 public class ElasticSearchDbIT extends AbstractJUnit4SpringContextTests {
 
 	@Autowired
