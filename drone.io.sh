d4f8a88d350028fbe59947f72cb2e1395685e071
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-01-30 18:06:50 +1100

    remove bad google config

--- drone.io.sh
+++ drone.io.sh
@@ -8,7 +8,7 @@ sudo apt-get update > /dev/null
 sudo apt-get purge google-chrome-unstable > /dev/null 2> /dev/null
 sudo apt-get install google-chrome-stable -y > /dev/null 2> /dev/null
 ls -l /usr/bin/google-chrome
-sudo rm -f /var/lib/apt/lists/dl.google.com_linux_chrome_deb_dists_stable_main_binary-i386_Packages
+sudo rm -f /etc/apt/sources.list.d/google.list
 
 echo install mlocate
 sudo apt-get update > /dev/null
