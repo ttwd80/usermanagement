0edebb55e080d733c2688f316b26ace29adc1242
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-01-30 18:49:28 +1100

    maven install

--- drone.io.sh
+++ drone.io.sh
@@ -24,43 +24,38 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add 
 echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-1.7.list > /dev/null
 sudo updatedb
 sudo apt-get update 2> /dev/null > /dev/null
-sudo apt-get install elasticsearch -y 
+sudo apt-get install elasticsearch -y  2> /dev/null > /dev/null
+echo | sudo tee -a /etc/default/elasticsearch 
+echo 'ES_JAVA_OPTS="-Djava.net.preferIPv4Stack=true"' | sudo tee -a /etc/default/elasticsearch 
 sudo service elasticsearch start
+sleep 10
 sudo service elasticsearch status
 
-# sudo apt-get purge openjdk-6-jdk openjdk-6-jre openjdk-6-jre-headless openjdk-6-jre-lib -y > /dev/null
-# sudo apt-get purge openjdk-7-jdk openjdk-7-jre openjdk-7-jre-headless openjdk-7-jre-lib -y > /dev/null
-# sudo apt-get autoremove -y 2> /dev/null > /dev/null
+echo purge openjdk
+sudo apt-get purge openjdk-6-jdk openjdk-6-jre openjdk-6-jre-headless openjdk-6-jre-lib -y > /dev/null
+sudo apt-get purge openjdk-7-jdk openjdk-7-jre openjdk-7-jre-headless openjdk-7-jre-lib -y > /dev/null
+sudo apt-get autoremove -y 2> /dev/null > /dev/null
 
 
-# echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-# echo debconf shared/accepted-oracle-license-v1-1 seen true |  sudo debconf-set-selections
+echo install oracle jdk
+debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+debconf shared/accepted-oracle-license-v1-1 seen true |  sudo debconf-set-selections
 
-# sudo apt-get install oracle-java8-installer -y 2> /dev/null > /dev/null
-# sudo apt-get update 2> /dev/null > /dev/null
-# sudo updatedb 2> /dev/null > /dev/null
+sudo apt-get install oracle-java8-installer -y 2> /dev/null > /dev/null
+sudo apt-get update 2> /dev/null > /dev/null
+sudo apt-get autoremove -y > /dev/null 2> /dev/null
+sudo updatedb 2> /dev/null > /dev/null
+java -version
 
-# 
+echo installing maven
+wget -q "http://apache.uberglobalmirror.com/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+tar zxf apache-maven-${MAVEN_VERSION}-bin.tar.gz
+sudo mv apache-maven-${MAVEN_VERSION} /opt
+rm apache-maven-${MAVEN_VERSION}-bin.tar.gz
+mvn --version
 
-# echo sudo apt-get autoremove -y > /dev/null 2> /dev/null
-
-# wget -q "http://apache.uberglobalmirror.com/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
-# tar zxf apache-maven-${MAVEN_VERSION}-bin.tar.gz
-
-# sudo mv apache-maven-${MAVEN_VERSION} /opt
-# rm apache-maven-${MAVEN_VERSION}-bin.tar.gz
-
-# echo | sudo tee -a /etc/default/elasticsearch 
-# echo 'ES_JAVA_OPTS="-Djava.net.preferIPv4Stack=true"' | sudo tee -a /etc/default/elasticsearch 
-
-# sudo service elasticsearch restart
-# sleep 10
-
-
-
-# chmod a+x init.sh
-
-# ./init.sh
+chmod a+x init.sh
+./init.sh
 
 
 
