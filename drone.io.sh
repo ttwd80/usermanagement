3db1a4fa9fbdd237b97c3c9556194c05a9c9458f
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-01-30 19:08:21 +1100

    test start xvfb

--- drone.io.sh
+++ drone.io.sh
@@ -18,9 +18,11 @@ locate -b stdio.h | head -1
 
 echo xvfb
 sudo updatedb
-locate vfb
-sudo apt-get install xvfb -y
+sudo apt-get install xvfb -y 2> /dev/null > /dev/null
+echo sudo sed -i "s/1024x768x24/1920x1024x32/g" /etc/init/xvfb.conf
 cat /etc/init/xvfb.conf
+sudo start xvfb
+sudo ps -ef | grep -i xvfb
 
 exit 1
 
