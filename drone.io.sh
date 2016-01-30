518c36176553978c491324decd2791c7618fb891
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-01-30 18:44:41 +1100

    elastic search installation seems to be ok

--- drone.io.sh
+++ drone.io.sh
@@ -17,17 +17,13 @@ sudo updatedb
 locate -b stdio.h | head -1
 
 echo adding elasticsearch repository
+sudo rm -rf `locate elasticsearch | egrep -v "/etc/apt/sources.list.d|/home"`
 gpg --keyserver pgpkeys.mit.edu --recv-key C90F9CB90E1FAD0C 2> /dev/null > /dev/null
 gpg --export --armor C90F9CB90E1FAD0C | sudo apt-key add - 2> /dev/null > /dev/null
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - > /dev/null
 echo "deb http://packages.elastic.co/elasticsearch/1.7/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-1.7.list > /dev/null
-sudo apt-get clean
-sudo apt-get update
 sudo updatedb
-sudo rm -rf `locate elasticsearch | egrep -v "/etc/apt/sources.list.d|/home"`
-sudo updatedb
-sudo apt-get update
-sudo apt-cache search elastic
+sudo apt-get update 2> /dev/null > /dev/null
 sudo apt-get install elasticsearch -y 
 sudo service elasticsearch start
 sudo service elasticsearch status
