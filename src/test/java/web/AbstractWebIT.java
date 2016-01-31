33bae7776137e36e40f34265d2cf69776a64515d
Titi Wangsa Damhore <ttwd80@gmail.com>
2016-02-01 04:06:57 +1100

    test full screen ui

--- src/test/java/web/AbstractWebIT.java
+++ src/test/java/web/AbstractWebIT.java
@@ -2,6 +2,7 @@ package web;
 
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Window;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
@@ -12,9 +13,11 @@ public abstract class AbstractWebIT extends AbstractJUnit4SpringContextTests {
 	protected WebDriver webDriver;
 
 	protected final String APPLICATION_PREFIX = "http://localhost:58080";
+	protected final int WINDOW_WIDTH = 1600;
 
 	@Before
 	public void setUp() {
-		webDriver.manage().window().maximize();
+		final Window window = webDriver.manage().window();
+		window.fullscreen();
 	}
 }
